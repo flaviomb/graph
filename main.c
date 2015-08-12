d438d7ed03dabd348c146a1ae739c2f360cbeabe
Flávio Mendes <eng.flavio94@gmail.com>
2015-08-12 15:51:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"adjmatrix.h"
+
 #define Option int
 
 int main()
@@ -13,35 +14,46 @@ int main()
 	scanf("%d", &V);
 	G = DIGRAPHinit(V);
 			
-	while(opt != 0)
+	while(opt != 5)
 	{
 		printf("\n==== Options ===== \n");
 		printf("\nOption 1: insert an arc. \n");
 		printf("\nOption 2: remove an arc.\n");
 		printf("\nOption 3: show the digraph\n");
-		printf("\nOption 4: quit\n");		
+		printf("\nOption 4: use dfs algorithm\n");
+		printf("\nOption 5: quit\n");		
 		printf("\nEnter the option: "); 
 		scanf("%d",&opt);	
 		
-		if(opt == 1)
+		switch(opt)
 		{
+		case(1):
 			printf("\n==== Option 1 selected (insert an arc) ====\n");
 			printf("\nEnter the initial vertex v and the final vertex w of the arc: ");
 			scanf("%d %d", &v, &w);
-			DIGRAPHinsertA(G,v,w);	
-		}
-		else if (opt == 2)
-		{
+			DIGRAPHinsertA(G,v,w);
+			break;	
+		case(2):
+		
 			printf("\n==== Option 2 selected (remove an arc) ====\n");
 			printf("\nEnter the initial vertex v and the final vertex w of the arc: ");
 			scanf("%d %d", &v, &w);
 			DIGRAPHremoveA(G,v,w);
-		}
-		else if (opt == 3)
-		{
+			break;
+		case(3):
+		
 			printf("\n==== Option 3 selected (show the digraph) ====\n");
 			DIGRAPHshow(G);
-		}
+			break;
+		case(4):
+			printf("\n==== Option 4 selected (use dfs algorithm) ====\n");
+			printf("\nEnter the initial vertex: ");
+			scanf("%d", &v);			
+			DIGRAPHdfs(G);
+			break;
+		default:
+			printf("\n==== Option 5 selected (quit) =====\n");
+		}	
 	}
 return 0;
 }
